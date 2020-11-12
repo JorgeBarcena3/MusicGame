5a5a59dca286402a1273de7d4f57c02308963273
Jorge Barcena Lumbreras <j.barcenalumbreras@gmail.com>
2020-11-12 20:16:11 +0100

    Player movement

---
+++ Assets/Script/DangerZone.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerZone : MonoBehaviour
+{
+    /// <summary>
+    /// Direction of launch
+    /// </summary>
+    private Vector2 launchDirection;
+
+    /// <summary>
+    /// Force
+    /// </summary>
+    [Range(0, 100)]
+    public float ejectForce = 1;
+
+    /// <summary>
+    /// When something start to collide the zone
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Player":
+
+                launchDirection = (Vector3.zero - collision.transform.position).normalized;
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
+    /// <summary>
+    /// When something is inside the zone
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Player":
+
+                Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+                if (rb)
+                    rb.AddForce(launchDirection * ejectForce);
+
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    /// <summary>
+    /// When something exit the zone
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Player":
+
+                Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+                if (rb)
+                    rb.AddForce(launchDirection * ejectForce * 2);
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Movement of the player
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// Speed of the player
+    /// </summary>
+    [Range(0, 10)]
+    public float speed = 1;
+
+    /// <summary>
+    /// Rigidbody of the player
+    /// </summary>
+    private Rigidbody2D rb;
+
+    /// <summary>
+    /// Store the direction of the player
+    /// </summary>
+    private Vector2 direction;
+
+    /// <summary>
+    /// Determines if the player has the movement control
+    /// </summary>
+    public bool hasControl = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Direction
+        direction = rb.velocity.normalized;
+
+        // Movement
+        if (hasControl)
+            rb.AddForce(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed);
+
+        //Rotation
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, -Vector3.forward);
+
+    }
+
+    /// <summary>
+    /// Gives the control to the player, in x time
+    /// </summary>
+    /// <param name="time"></param>
+    /// <param name="control"></param>
+    /// <returns></returns>
+    public IEnumerator retrunsControl(float time,  bool control = true)
+    {
+        yield return new WaitForSeconds(time);
+        hasControl = control;
+    }
+
+
+}
