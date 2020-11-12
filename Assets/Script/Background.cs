f206c1c264e8b0bcfe4d6179428f5c77b9050ea0
Jorge Barcena Lumbreras <j.barcenalumbreras@gmail.com>
2020-11-12 21:37:32 +0100

    background

---
+++ Assets/Script/Background.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    /// <summary>
+    /// Start in the background
+    /// </summary>
+    private List<GameObject> stars = new List<GameObject>();
+
+    /// <summary>
+    /// Number of stars
+    /// </summary>
+    public int startsNumber;
+
+    /// <summary>
+    /// Star prefab
+    /// </summary>
+    public GameObject star;
+
+    /// <summary>
+    /// Direction of the background
+    /// </summary>
+    public Vector2 direction;
+
+    /// <summary>
+    /// Height limit
+    /// </summary>
+    public Vector2 height;
+
+    /// <summary>
+    /// Width limit
+    /// </summary>
+    public Vector2 width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < startsNumber; i++)
+        {
+            stars.Add(Instantiate(star, this.transform));
+            stars.Last().GetComponent<Star>().setBackgroundManager(this);
+        }
+    }
+
+    /// <summary>
+    /// Returns the direction flow
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 getDirection()
+    {
+        return direction.normalized;
+    }
+}
---
+++ Assets/Script/Star.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    /// <summary>
+    /// Star speed
+    /// </summary>
+    private float speed;
+
+    /// <summary>
+    /// Background manager
+    /// </summary>
+    private Background bg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(0.05f, 0.25f);
+    }
+
+    public void setBackgroundManager(Background _bg)
+    {
+        bg = _bg;
+        transform.position = new Vector3(Random.Range(bg.width.x, bg.width.y), bg.height.y, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.y < bg.height.x)
+        {
+            transform.position = new Vector3(Random.Range(bg.width.x, bg.width.y), bg.height.y, 0);
+
+        }
+        else
+            transform.position = transform.position + (bg.getDirection() * speed * 0.15f);
+
+    }
+}
